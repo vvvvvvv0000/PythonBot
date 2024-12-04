13187c217d4fd714cb13a8c2fd433a516933ed7b
vvvvvvv0000 <l9425131@gmail.com>
2024-12-04 19:25:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,33 @@
-#7757066965:AAH5ud-dxH5efI70xQZihC2bOJYiiAS47r0
+# 7757066965:AAH5ud-dxH5efI70xQZihC2bOJYiiAS47r0
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, ConversationHandler, \
+from telegram import InputMediaPhoto, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, ConversationHandler,
     MessageHandler, filters
 
 app = ApplicationBuilder().token('7757066965:AAH5ud-dxH5efI70xQZihC2bOJYiiAS47r0').build()
 
+
 async def services_command(update, context):
-    services_text = ( "Услуги салона:\n"
-                      "/1.Маникюр\n"
-                      "/2.Макияж\n"
-                      "/3.Прически"
-                      )
+    services_text = ("Услуги салона:\n"
+                     "/1.Маникюр\n"
+                     "/2.Макияж\n"
+                     "/3.Прически"
+                     )
     await update.message.reply_text(services_text)
     return DATE_START
 
+
 async def prays_command(update, context):
-    prays_text = ( "Прайс салона:\n"
-                      "/1.Маникюр-1000\n"
-                      "/2.Макияж-2000\n"
-                      "/3.Прически-2500\n"
-                      )
+    prays_text = ("Прайс салона:\n"
+                  "/1.Маникюр-1000\n"
+                  "/2.Макияж-2000\n"
+                  "/3.Прически-2500\n"
+                  )
     await update.message.reply_text(prays_text)
 
+
 async def vremya_raboty_command(update, context):
-    vremya_raboty_text = ( "Время работы салона:\n"
+    vremya_raboty_text = ("Время работы салона:\n"
                           "/Понедельник-Субота 8:00-21:00\n"
                           "/Воскресенье-выходной"
                           )
@@ -33,6 +36,8 @@ async def vremya_raboty_command(update, context):
 
 # Стадії конверсії
 DATE_START, DATE_END, VREMYA, TYPE = range(4)
+
+
 async def button_handler(update, context):
     query = update.callback_query
     await query.answer()
@@ -41,10 +46,18 @@ async def button_handler(update, context):
         await query.message.reply_text(
             "Что бы узнать о нас,отправьте следующую информацию:\n"
             "-services - узнать об услугах\n"
-        "prays - узнать цену\n "
-        "vremya_raboty - узнать время работы"
+            "prays - узнать цену\n "
+            "vremya_raboty - узнать время работы"
         )
     elif query.data == "services":
+        room_image_url = "image/Blend makeup.png"
+        caption = "салон"
+        try:
+            await query.message.reply_photo(photo=room_image_url, caption=caption)
+        except FileNotFoundError as e:
+            await query.message.reply_text(f"Помилка: файл {e.filename} не знайдено.")
+        except Exception as e:
+            await update.message.reply_text(f"Виникла помилка: {str(e)}")
         await query.message.reply_text(
             "Услуги салона:\n"
             "/1.Маникюр\n"
@@ -52,11 +65,20 @@ async def button_handler(update, context):
             "/3.Прически"
         )
     elif query.data == "prays":
+
         await query.message.reply_text(
-            "Прайс салона:\n"
-             "/1.Маникюр-1000\n"
-             "/2.Макияж-2000\n"
-             "/3.Прически-2500\n"
+            room_image_url="image/makeup.jpg, image/manicure.jpg, image/peducure.jpg"
+        caption = "макияж, маникюр, педикюр"
+        try:
+            await query.message.reply_photo(photo=room_image_url, caption=caption)
+        except FileNotFoundError as e:
+            await query.message.reply_text(f"Помилка: файл {e.filename} не знайдено.")
+        except Exception as e:
+            await update.message.reply_text(f"Виникла помилка: {str(e)}")
+             "Прайс салона:\n"
+            "/1.Маникюр-1000\n"
+            "/2.Макияж-2000\n"
+            "/3.Прически-2500\n"
         )
     elif query.data == "vremya_raboty":
         await query.message.reply_text(
@@ -65,56 +87,69 @@ async def button_handler(update, context):
             "/Воскресенье-выходной"
         )
 
+
 async def start_command(update, context):
-       inline_keyboard = [
-           [InlineKeyboardButton("Услуги салона", callback_data= "services")],
-           [InlineKeyboardButton("Прайс салона", callback_data= "prays")],
-           [InlineKeyboardButton("Время работы салона", callback_data= "vremya_raboty" )]
-       ]
+    inline_keyboard = [
+        [InlineKeyboardButton("Услуги салона", callback_data="services")],
+        [InlineKeyboardButton("Прайс салона", callback_data="prays")],
+        [InlineKeyboardButton("Время работы салона", callback_data="vremya_raboty")]
+    ]
 
-       markup = InlineKeyboardMarkup(inline_keyboard)
+    markup = InlineKeyboardMarkup(inline_keyboard)
 
-       await update.message.reply_text(
-            "Добро пожаловать в салон красоты  'Айлен'\n"
+    await update.message.reply_text(
+        "Добро пожаловать в салон красоты  'Айлен'\n"
         "Я помогу вам с информацией о наших услугах.\n"
         "Попробуйте наши следующие команды:\n"
         "/services - узнать об услугах\n"
         "/prays - узнать цену\n "
         "/vremya_raboty - узнать время работы"
-         "Что бы узнать о нас,отправьте следующую информацию:",
-         reply_markup=markup
-     )
-       
+        "Что бы узнать о нас,отправьте следующую информацию:",
+        reply_markup=markup
+    )
+
+
 app.add_handler(CallbackQueryHandler(button_handler))
+
+
 async def date_start(update, context):
-     context.user_data['date_start']=update.message.text
-     await update.message.reply_text("Введите дату записи (например, 2028-10-11):")
-     return DATE_END
+    context.user_data['date_start'] = update.message.text
+    await update.message.reply_text("Введите дату записи (например, 2028-10-11):")
+    return DATE_END
+
+
 async def date_end(update, context):
-     context.user_data['date_end'] = update.message.text
-     await update.message.reply_text("На которое время (например, 14:00):")
-     return VREMYA
+    context.user_data['date_end'] = update.message.text
+    await update.message.reply_text("На которое время (например, 14:00):")
+    return VREMYA
+
+
 async def type_uslug(update, context):
-     context.user_data['type_uslug'] = update.message.text
-     reply_keyboard = [["Визажист", "Маникюрша", "Педикюрша"]]
-     markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
-     await update.message.reply_text("Выберите тип услуги:", reply_markup=markup)
-     return  TYPE
+    context.user_data['type_uslug'] = update.message.text
+    reply_keyboard = [["Визажист", "Маникюрша", "Педикюрша"]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+    await update.message.reply_text("Выберите тип услуги:", reply_markup=markup)
+    return TYPE
+
+
 async def data_type(update, context):
-     context.user_data['data_type'] = update.message.text
-     booking_details = (
-         f"Ваши данные для записи:\n"
-         f"-Дата записи: {context.user_data['date_start']}\n"
-         f"-Время: {context.user_data['date_end']}\n"
-         f"-Тип услуги: {context.user_data[' type_uslug']}\n"
-         "Если все верно, то наш менеджер свяжеться с вами для подтверждения"
-     )
-     await update.message.reply_text(booking_details, reply_markup=ReplyKeyboardRemove())
-     return ConversationHandler.END
+    context.user_data['data_type'] = update.message.text
+    booking_details = (
+        f"Ваши данные для записи:\n"
+        f"-Дата записи: {context.user_data['date_start']}\n"
+        f"-Время: {context.user_data['date_end']}\n"
+        f"-Тип услуги: {context.user_data[' type_uslug']}\n"
+        "Если все верно, то наш менеджер свяжеться с вами для подтверждения"
+    )
+    await update.message.reply_text(booking_details, reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
 
 async def cancel(update, context):
-    await update.message.reply_text("Бронирование отменено. Возвращайтесь, когда будете готовы!", reply_markup=ReplyKeyboardRemove())
+    await update.message.reply_text("Бронирование отменено. Возвращайтесь, когда будете готовы!",
+                                    reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
+
 
 app.add_handler(CommandHandler("start", start_command))
 
@@ -129,6 +164,36 @@ booking_handler = ConversationHandler(
     },
     fallbacks=[CommandHandler("cancel", cancel)],
 )
+
+async def send_photos(update, context):
+    photo_paths = ["image/Blend makeup.png", "image/makeup.jpg", "image/manicure.jpg", "image/peducure.jpg"]
+    try:
+        media_group = [InputMediaPhoto(open(photo, "rb")) for photo in photo_paths]
+        await update.message.reply_media_group(media_group)
+        await update.message.reply_text("Приклад")
+    except FileNotFoundError as e:
+        await update.message.reply_text(f"Помилка: файл {e.filename}не знайдено.")
+    except Exception as e:
+        await update.message.reply_text(f"Виникла помилка: {str(e)}")
+
+
+app.add_handler(CommandHandler("sendphotos", send_photos))
+app.add_handler(booking_handler)
+
+
+
+async def sales(update, context):
+    room_image_url = "image/"
+    caption = "ответ на ваш запрос"
+    try:
+        await update.message.reply_photo(photo=room_image_url, caption=caption)
+    except FileNotFoundError as e:
+        await update.message.reply_text(f"Помилка: файл {e.filename} не знайдено.")
+    except Exception as e:
+        await update.message.reply_text(f"Виникла помилка: {str(e)}")
+
+app.add_handler(CommandHandler("sendphotos", send_photos))
+app.add_handler(CommandHandler("sales", sales))
 
 app.add_handler(booking_handler)
 
